2d234955115ece8190c4ceba03adf8b04fc4ff4a
doca41531 <doca41531@gmail.com>
2023-11-26 17:10:45 +0900

    둠

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,12 @@
 "use strict"
+const id = document.querySelector("#id")
+const pw = document.querySelector("#pw")
+const Btn = document.querySelector("button")
 
-console.log("hello");
+Btn.addEventListener("click", () => {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    }
+    console.log(req)
+})
