8a3a49f28c351e58b534a720d546104977b78dd5
doca41531 <doca41531@gmail.com>
2023-11-26 17:16:15 +0900

    fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,5 +8,12 @@ Btn.addEventListener("click", () => {
         id: id.value,
         pw: pw.value,
     }
-    console.log(req)
+    
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(req),
+    })
 })
